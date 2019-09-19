3321987cd1868f910dab98a5fa5ce36acfd1f6cf
elyalvarado <elyalvarado@gmail.com>
2019-09-19 09:19:36 -0500

    Rename variables to one letter

--- solution.rb
+++ solution.rb
@@ -1,51 +1,51 @@
-def santa_time elves, serialized_houses
-  houses = serialized_houses.split /\+|\n/
-  houses.map { |house|
-    naughty_people, good_people = house.split('*').map(&:to_i)
-    good_people ||= 0
-    naughty_people ||= 0
-    return -1 if (good_people > 0 && elves < 2) ||(naughty_people > 0 && elves < 3)
-    next 0 if elves == 0
-    all_jobs = [1]*naughty_people + [0]*good_people
-    all_possible_order_of_jobs = all_jobs.permutation
-    finish_times = all_possible_order_of_jobs.map do |job_list|
-      jobs = job_list.map { |x| x==1 ? [3,5] : [2,4] }
-      current_height = [0] * elves
-      jobs.each do |package|
-        package_width, package_height = package
-        spaces_available_to_the_right = current_height.map.with_index do |x,i|
+def santa_time e, h
+  h = h.split /\+|\n/
+  h.map { |h|
+    n, g = h.split('*').map(&:to_i)
+    g ||= 0
+    n ||= 0
+    return -1 if (g > 0 && e < 2) ||(n > 0 && e < 3)
+    next 0 if e == 0
+    v = [1]*n + [0]*g
+    o = v.permutation
+    f = o.map do |j|
+      j = j.map { |x| x==1 ? [3,5] : [2,4] }
+      c = [0] * e
+      j.each do |q|
+        w, y = q
+        r = c.map.with_index do |x,i|
           a = 0
-          current_height[i..elves].each { |r| r <= x ? a+=1 : break }
+          c[i..e].each { |r| r <= x ? a+=1 : break }
           a
         end
-        spaces_available_to_the_left = current_height.map.with_index do |x,i|
+        l = c.map.with_index do |x,i|
           a = 0
-          current_height[0..i].reverse.each { |r| r <= x ? a+=1 : break }
+          c[0..i].reverse.each { |r| r <= x ? a+=1 : break }
           a
         end
-        all_columns_with_space_available_to_the_right =
-            spaces_available_to_the_right.each.with_index.inject([]) { |acc,(x,i)|
-              acc<<i if x >= package_width
+        a =
+            r.each.with_index.inject([]) { |acc,(x,i)|
+              acc<<i if x >= w
               acc
             }
-        column_with_spaces_available_to_the_right_and_min_height =
-            all_columns_with_space_available_to_the_right.min { |a,b|
-              current_height[a] <=> current_height[b]
+        d =
+            a.min { |a,b|
+              c[a] <=> c[b]
             }
-        bottom_left_column =
-            column_with_spaces_available_to_the_right_and_min_height -
-                (spaces_available_to_the_left[
-                    column_with_spaces_available_to_the_right_and_min_height
+        b =
+            d -
+                (l[
+                    d
                 ] - 1)
-        new_height_for_affected_columns = current_height[
-            column_with_spaces_available_to_the_right_and_min_height
-        ] + package_height
-        (bottom_left_column..(bottom_left_column+package_width-1)).each { |x|
-          current_height[x] = new_height_for_affected_columns
+        z = c[
+            d
+        ] + y
+        (b..(b+w-1)).each { |x|
+          c[x] = z
         }
       end
-      current_height.max
+      c.max
     end
-    finish_times.min
+    f.min
   }.sum
 end
