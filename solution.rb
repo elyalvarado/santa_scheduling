3bd7a0c765d388a0c525602fd2e2b205f91d29a2
elyalvarado <elyalvarado@gmail.com>
2019-09-19 09:56:22 -0500

    Replaced each by map

--- solution.rb
+++ solution.rb
@@ -4,12 +4,12 @@
     return -1 if (g > 0 && e < 2) ||(n > 0 && e < 3)
     ([[3,5]]*n + [[2,4]]*g).permutation.map { |j|
       c = [0] * e
-      j.each { |q|
+      j.map { |q|
         w, y = q
         i=j=0
         r = c.map { |x|
           a = b = 0
-          c[i..e].each { |r| r <= x ? a+=1 : break }
+          c[i..e].map { |r| r <= x ? a+=1 : break }
           (t=i+=1).times { c[t-=1] <= x ? b+=1 : break }
           [a,b]
         }
@@ -21,7 +21,7 @@
         d = a.min { |a,b| c[a] <=> c[b] }
         b = d - (r[d][1] - 1)
         z = c[d] + y
-        (b..(b+w-1)).each { |x| c[x] = z }
+        (b..(b+w-1)).map { |x| c[x] = z }
       }
       c.max
     }.min||0
